c3de0aaa4c3d89546661d038efc13e0ba3dd28fa
Jason Fu <jasonfu1992@gmail.com>
2019-01-12 10:19:30 -0800

    Add W2D1 simon homework

---
+++ W2D1/simon/lib/simon.rb
@@ -0,0 +1,74 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+    take_turn until game_over
+    game_over_message
+    reset_game
+    puts "Play again? (y/n)"
+    play if (gets.chomp.downcase == "y")
+  end
+
+  def take_turn
+    show_sequence
+    require_sequence
+    unless game_over
+      round_success_message
+      self.sequence_length += 1
+    end
+  end
+
+  def show_sequence
+    add_random_color
+    puts "The current sequence will be displayed for #{sequence_length} seconds:"
+    puts seq.join(",")
+    sleep(sequence_length)
+    system("clear")
+  end
+
+  def require_sequence
+    puts "Enter the colors in sequence, each followed by a comma:"
+    input = gets.chomp.split(",").each(&:downcase)
+    check_sequence(input)
+  end
+
+  def add_random_color
+    self.seq.push(COLORS.sample)
+  end
+
+  def round_success_message
+    puts "You entered the correct sequence! Next round..."
+  end
+
+  def game_over_message
+    puts "Incorrect move! You lose."
+  end
+
+  def reset_game
+    self.sequence_length = 1
+    self.game_over = false
+    self.seq = []
+  end
+
+  def check_sequence(input)
+    seq.each_index do |idx|
+      if input[idx] != seq[idx]
+        self.game_over = true 
+        return
+      end
+    end
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  game = Simon.new
+  game.play
+end
